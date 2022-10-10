88fc29ad3b5758c64c7b96b7f541ae5bbe542be1
Ondairos <ondairos@gmail.com>
2022-10-10 13:51:13 +0300

    Starter files for express app

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require ('express');
+
+const app = express  ();
+
+app.get('/', (req, res, next) => {
+    res.render();
+});
+
+app.get('/users', (req, res, next) => {
+    res.render();
+});
+
+
+app.post('/add-user',(req,res,next) =>{
+    res.redirect('/users');
+});
+
+
+
+app.listen(3000);
